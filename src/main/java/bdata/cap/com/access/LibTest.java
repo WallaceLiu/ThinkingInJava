c42c3a991db24ddd7cc80fdcc22863c3a0c53e59
cap <cap@capdeMacBook-Pro.local>
2019-11-24 21:56:42 +0800

    fixed

--- src/main/java/bdata/cap/com/access/LibTest.java
+++ src/main/java/bdata/cap/com/access/LibTest.java
@@ -2,16 +2,14 @@
 // Uses the library.
 package bdata.cap.com.access;
 
-//import bdata.cap.com.net.mindview.simple.*;
-
 import mindview.simple.List;
 import mindview.simple.Vector;
 
 public class LibTest {
-  public static void main(String[] args) {
-    Vector v = new Vector();
-    List l = new List();
-  }
+    public static void main(String[] args) {
+        Vector v = new Vector();
+        List l = new List();
+    }
 } /* Output:
 net.mindview.simple.Vector
 net.mindview.simple.List
