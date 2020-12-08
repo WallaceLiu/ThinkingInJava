98d2db892478db5f7d48d23c761c21d109b5c713
liuning11 <liuning11@jd.com>
2020-12-09 00:50:08 +0800

    fixed

---
+++ src/test/java/com/bdata/cap/Test.java
@@ -0,0 +1,17 @@
+package com.bdata.cap;
+
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class Test {
+    @org.junit.Test
+    public void t() {
+        Object s = "[0.006493506493506494]";
+        List l = (List) s;
+        System.out.println("aaa");
+    }
+}
